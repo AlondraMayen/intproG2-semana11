42d3cd21e8e549dd7cdd12e8c60054157bb61fb0
AlondraMayen <asmayen@uamv.edu.ni>
2025-06-03 13:43:26 -0600

    ejercicio2

---
+++ funciones_practica/ejercicio2.py
@@ -0,0 +1,13 @@
+# Saludar a alguien definiendo el nombre
+def saludar (nombre):
+    print("Hola", nombre)
+    
+llamada= saludar("Alondra")
+print(llamada)
+
+# Saludar a alguien sin definir el nombre
+def saludar (nombre):
+    return()
+
+nombre= input ("Ingrese su nombre: ")
+print("Hola", nombre)
