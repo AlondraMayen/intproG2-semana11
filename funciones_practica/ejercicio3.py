979b0a8d328e0aea9e7b8faf8f3b28842e1148d5
AlondraMayen <asmayen@uamv.edu.ni>
2025-06-03 13:43:51 -0600

    ejercicio3

---
+++ funciones_practica/ejercicio3.py
@@ -0,0 +1,7 @@
+# Media de los tres ultimos examenes
+def media(num1,num2,num3):
+    resultado = (num1 + num2 + num3)/3
+    return resultado # Devolver, siempre va acompañado de un valor
+
+llamada = media(2, 2, 5) # Argumentos 
+print(llamada)
