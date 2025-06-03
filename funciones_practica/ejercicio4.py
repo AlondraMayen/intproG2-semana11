b94aa4f3137c25d119273927791c2e2feb8a3630
AlondraMayen <asmayen@uamv.edu.ni>
2025-06-03 14:07:57 -0600

    ejercicio4

---
+++ funciones_practica/ejercicio4.py
@@ -0,0 +1,8 @@
+# Parametro, Argumento y Variable
+# Escribe una función doble que reciba un número y devuelva el doble de ese número.
+
+def doble (numero):
+    return numero*2
+
+resultado = doble (int(input("Ingrese un numero: ")))
+print ("El doble de numero es:", resultado)
